43641eb684ea5153397d89dce4bd13205db6fa17
blackcatpopo <souki0422_msw@yahoo.co.jp>
2017-04-23 23:59:22 +0900

    first commit

---
+++ autoinput.js
@@ -0,0 +1,8 @@
+﻿document.querySelector( "input[name='sex']" ).checked = true;
+document.querySelector( "input[name='birth_year']" ).value = "1993";
+document.querySelector( "select[name='prefectures'] option[value='27']" ).selected = true;
+document.querySelector( "select[name='job'] option[value='2']" ).selected = true;
+document.querySelector("input[name='location']").checked= true;
+var form = document.querySelector("form[name='form1']");
+form.action = "form_ins4-sp.php";
+form.submit();
